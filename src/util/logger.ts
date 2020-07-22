69d5f5851e9b092c2b5aad0c3c6e72e386b851aa
Jarrod Overson <jsoverson@gmail.com>
2020-07-21 22:35:55 -0400

    fixed log methods not formatting like debug

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,22 +1,45 @@
 import DEBUG, { Debugger } from 'debug';
 import chalk from 'chalk';
 
+const { formatArgs } = require('debug');
+
+function format(...args: any) {
+  return args[0] || '';
+}
+
 export default class Logger {
   debug: Debugger;
 
   constructor(name: string) {
     this.debug = DEBUG(name);
   }
+  format(...args: any) {
+    let index = 0;
+    args[0] = args[0].replace(/%([a-zA-Z%])/g, (match: string, format: string) => {
+      if (match === '%%') return '%';
+      index++;
+      const formatter = DEBUG.formatters[format];
+      if (typeof formatter === 'function') {
+        const val = args[index];
+        match = formatter.call(this.debug, val);
+        args.splice(index, 1);
+        index--;
+      }
+      return match;
+    });
+
+    return args[0] || '';
+  }
   print(...args: any) {
     console.log(...args);
   }
   info(...args: any) {
-    console.log(chalk.cyan('Info: '), ...args);
+    console.log(chalk.cyan('Info: ') + this.format(...args));
   }
   warn(...args: any) {
-    console.warn(chalk.yellow('Warning: '), ...args);
+    console.log(chalk.yellow('Warning: ') + this.format(...args));
   }
   error(...args: any) {
-    console.error(chalk.red('Error: '), ...args);
+    console.log(chalk.red('Error: ') + this.format(...args));
   }
 }
